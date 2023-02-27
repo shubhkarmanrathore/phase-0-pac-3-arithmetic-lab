21ad7f410b926edaa9468a340d38c39d52a56177
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-02-27 13:45:09 -0800

    adds arithmetic lab

---
+++ index.js
@@ -0,0 +1,26 @@
+function add (a, b) {
+    return a + b;
+} 
+function subtract (a, b) {
+    return a - b;
+}
+function multiply (a, b) {
+    return a * b;
+}
+function divide (a, b) {
+    return a / b;
+}
+let n=22
+function increment (n) {
+    return n+=1;
+}
+function decrement (n) {
+    return n-=1;
+}
+let string = 52.36
+function preserveDecimal (string) {
+    return parseFloat(string, 10);
+}
+function makeInt (string) {
+    return parseInt (string, 10);
+}
